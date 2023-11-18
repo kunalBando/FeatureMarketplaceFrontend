dafb26dcf7d205dd8612e00c90c1c04d025ac795
kunalBando <144883534+kunalBando@users.noreply.github.com>
2023-11-18 10:55:23 +0530

    Update UserHomePage.js

--- src/Pages/UserHomePage.js
+++ src/Pages/UserHomePage.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "../Design/UserHome.css";
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-import UserNavbar from "../Components/UserNavbar";
+
 
 const GridBox = ({ data }) => {
   return (
