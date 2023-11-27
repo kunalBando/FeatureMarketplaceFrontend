e621ef31a06c329c43371cdee0f0f734d04eb2fb
kunalBando <144883534+kunalBando@users.noreply.github.com>
2023-11-27 15:27:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import Login from "./Components/Login";
-import UserHomePage from "./Pages/UserHomePage";
-import CustomSearchPage from "./Pages/CustomSearchPage";
-import AddFeaturePage from "./Pages/AddFeaturePage";
+
 
 //import Footer from "./Components/Footer";
 //import nav from "bootstrap";
@@ -17,10 +15,7 @@ function App() {
         
         <Routes>
           <Route path="/" element={<LandingPage/>}/>
-          <Route path="/userlogin" element={<Login/>}/>
-          <Route path="/userHome" element={<UserHomePage/>}/>
-          <Route path="/customsearch" element={<CustomSearchPage/>}/>
-          <Route path="/addfeature" element={<AddFeaturePage/>}/>
+          <Route path="/userlogin" element={<Login/>}/>          
         </Routes>
       </BrowserRouter>
       
